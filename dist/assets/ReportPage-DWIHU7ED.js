af0d6c1ce41cddbf489d75f2913e846c333cc68a
Jhoseto <serezliev@gmail.com>
2026-03-06 13:48:16 +0200

    Update index.html and remove deprecated assets for improved performance
    - Updated script references in index.html to enhance loading efficiency.
    - Removed obsolete JavaScript files related to ArchivePage, ExpensesPage, and ChatBotAdminPage to reduce bundle size and improve application performance.
    - Adjusted asset paths to ensure proper loading of necessary resources.

--- dist/assets/ReportPage-DWIHU7ED.js
+++ dist/assets/ReportPage-DWIHU7ED.js
@@ -1,4 +1,4 @@
-import{j as n}from"./motion-BZnCyKgS.js";import{r as j,b as we,d as Ua,f as Fa,u as ma,j as xa}from"./react-vendor-DfPuibdP.js";import{m as ws,g as va,a as dr,s as Ea}from"./archiveService-CvRaEaC-.js";import{u as he,a as ar,s as ba}from"./index-DOw2N-Zg.js";import{T as Ha,S as aA,R as Ia,g as Qs,a as ya}from"./ReliabilityGauge-O4K-GQSc.js";import{R as Ka,a as La,P as Na,b as Sa,T as Da,d as Ta}from"./charts-DwoTjIGi.js";import"./firebase-DzH6GH7r.js";/*!
+import{j as n}from"./motion-BZnCyKgS.js";import{r as j,b as we,d as Ua,f as Fa,u as ma,j as xa}from"./react-vendor-DfPuibdP.js";import{m as ws,g as va,a as dr,s as Ea}from"./archiveService-CDtVtYly.js";import{u as he,a as ar,s as ba}from"./index-Ba7kS2RH.js";import{T as Ha,S as aA,R as Ia,g as Qs,a as ya}from"./ReliabilityGauge-6IN_zypB.js";import{R as Ka,a as La,P as Na,b as Sa,T as Da,d as Ta}from"./charts-DwoTjIGi.js";import"./firebase-DzH6GH7r.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
