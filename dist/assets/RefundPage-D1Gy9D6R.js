265cf8d4b5245f183e989af6891f5b5eb22d9afc
Jhoseto <serezliev@gmail.com>
2026-03-23 09:33:18 +0200

    chore: Update welcome bonus points to 500 and adjust asset references in index.html while removing obsolete JavaScript files from distribution

--- dist/assets/RefundPage-D1Gy9D6R.js
+++ dist/assets/RefundPage-D1Gy9D6R.js
@@ -1,4 +1,4 @@
-import{j as r}from"./motion-BZnCyKgS.js";import{u as t,H as o}from"./index-om89rlg9.js";import{r as a}from"./renderLegalMd-4mZxAyAc.js";import{L as s}from"./react-vendor-DfPuibdP.js";import"./firebase-DzH6GH7r.js";import"./charts-DwoTjIGi.js";const i=`# ПОЛИТИКА ЗА ВРЪЩАНЕ НА СУМИ – FACTCHECKER AI\r
+import{j as r}from"./motion-BZnCyKgS.js";import{u as t,H as o}from"./index-DHUobmZA.js";import{r as a}from"./renderLegalMd-4mZxAyAc.js";import{L as s}from"./react-vendor-DfPuibdP.js";import"./firebase-DzH6GH7r.js";import"./charts-DwoTjIGi.js";const i=`# ПОЛИТИКА ЗА ВРЪЩАНЕ НА СУМИ – FACTCHECKER AI\r
 \r
 **Последна актуализация:** Март 2026 г.\r
 \r
