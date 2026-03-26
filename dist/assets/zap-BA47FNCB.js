ea409dfee464614e84c1199f250317ad566b736f
Jhoseto <serezliev@gmail.com>
2026-03-26 10:39:20 +0200

    chore: update asset references in index.html, remove obsolete JavaScript and CSS files, and clean up sitemap.xml
    - Updated script and stylesheet links in index.html to reflect new asset versions.\n- Removed outdated JavaScript files related to ArchivePage, ExpensesPage, FAQPage, FeaturesPage, and HowItWorksPage.\n- Cleaned up sitemap.xml by removing an obsolete URL entry.

--- dist/assets/zap-BA47FNCB.js
+++ dist/assets/zap-BA47FNCB.js
@@ -1,4 +1,4 @@
-import{e as a}from"./index-BuA16MJd.js";/**
+import{e as a}from"./index-CYkfPOfK.js";/**
  * @license lucide-react v0.575.0 - ISC
  *
  * This source code is licensed under the ISC license.
